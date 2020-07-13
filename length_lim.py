ebe0d86a8bc49f712fc0d797b7b29a9a3bd75045
penrin <hrs@penr.in>
2020-07-13 11:35:52 +0900

    minor changes

--- length_lim.py
+++ length_lim.py
@@ -8,23 +8,24 @@ def calc_lengthlim(ws, fs, ch):
     else:
         return np.floor((2 ** 32 - 1) / ws / ch)
 
+if __name__ == '__main__':
 
-ws, fs = 3, 48000
-ch = [1, 2, 24, 96, 288, 1024, min(2 ** 16 - 1, np.floor(2 ** 32 / 3 / 48000))]
-L = np.empty(len(ch), dtype=np.int)
-for i in range(len(ch)):
-    L[i] = calc_lengthlim(ws, fs, ch[i])
-    print(ch[i], L[i] / fs)
+    ws, fs = 3, 48000
+    ch = [1, 2, 24, 96, 288, 1024, min(2 ** 16 - 1, np.floor(2 ** 32 / 3 / 48000))]
+    L = np.empty(len(ch), dtype=np.int)
+    for i in range(len(ch)):
+        L[i] = calc_lengthlim(ws, fs, ch[i])
+        print(ch[i], L[i] / fs)
 
-plt.plot(ch, L / fs, '.-')
-plt.xscale('log')
-plt.yscale('log')
+    plt.plot(ch, L / fs, '.-')
+    plt.xscale('log')
+    plt.yscale('log')
 
-plt.xlabel('Number of channels')
-plt.ylabel('Time (sec)')
-plt.xlim(1, 2 ** 17)
-plt.grid(which='both', lw=0.5)
-plt.show()
+    plt.xlabel('Number of channels')
+    plt.ylabel('Time (sec)')
+    plt.xlim(1, 2 ** 17)
+    plt.grid(which='both', lw=0.5)
+    plt.show()
 
 
 
